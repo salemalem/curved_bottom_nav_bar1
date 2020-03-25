cf4539624acd749bc4caef7875ef46e149ce2d9b
salemalem <wymedia@protonmail.com>
2020-03-25 19:04:30 +0600

    UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+
+// for curved navigation bar
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-void main() => runApp(MaterialApp(home: BottomNavBar()));
+void main() => runApp(
+    MaterialApp(
+      home: BottomNavBar(),
+    )
+);
 
 class BottomNavBar extends StatefulWidget {
   @override
@@ -15,29 +21,39 @@ class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          key: _bottomNavigationKey,
-          index: 0,
-          height: 50.0,
-          items: <Widget>[
-            Icon(Icons.add, size: 30),
-            Icon(Icons.list, size: 30),
-            Icon(Icons.compare_arrows, size: 30),
-            Icon(Icons.call_split, size: 30),
-            Icon(Icons.perm_identity, size: 30),
-          ],
-          color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Colors.blueAccent,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (index) {
-            setState(() {
-              _page = index;
-            });
-          },
-        ),
-        body: bodyWidget(_page)
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 50.0,
+        items: <Widget>[
+          Icon(
+            Icons.search,
+            size: 30,
+            color: Colors.white70,
+          ),
+          Icon(
+            Icons.library_music,
+            size: 30,
+            color: Colors.white70,
+          ),
+          Icon(
+            Icons.free_breakfast,
+            size: 30,
+            color: Colors.white70,
+          )
+        ],
+        color: Colors.black,
+        buttonBackgroundColor: Colors.green[500],
+        backgroundColor: Colors.grey[900],
+        animationCurve: Curves.easeOut,
+        animationDuration: Duration(milliseconds: 300),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      body: bodyWidget(_page),
     );
   }
 }
@@ -66,4 +82,16 @@ class Page2 extends StatelessWidget {
       color: Colors.white,
     );
   }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
